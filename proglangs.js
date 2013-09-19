1c8f8bbcc49bd353f22ef2ddaf8930a7343a633c
Bergem93 <bergem@live.no>
2013-09-19 12:49:45 +0200

    Laboppgave 2
    Lagt til kode for laboppgave 2

--- proglangs.js
+++ proglangs.js
@@ -2,3 +2,10 @@
         document.getElementById("resultat1").innerHTML = value; // henter resultatet fra select og endrer <div>
 		document.getElementById("resultat2").innerHTML = "Gratulerer, du valgte:";
     }
+
+
+function AlertIt() {
+var answer = confirm ("Siden er under konstruksjon, biaaatch!.")
+if (answer)
+window.location="proglangs2.html";
+}
